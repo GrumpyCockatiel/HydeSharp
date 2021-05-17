560a63914e7518cac3c21cf35ac0923206cd3471
Grumpy Cockatiel <tguillory@gmail.com>
2021-05-17 13:54:56 -0500

    working on the processor

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Hyde Sharp Project
 
+Hyde Sharp is just an experiment to recreate Jekyll in C# .NET Core.
 
+I just want a simple way to build static websites from Markdown files and HTML templates.
+
+I have other ideas for later, but this is just to get the ball rolling.
+
+## Folder Structure
+
+I'm trying to echo the Jekyll folder structure for compatibility.
+
+## Dependencies
+
+For now my own lib [Raydreams.Common](https://github.com/GrumpyCockatiel/Raydreams.Common) is a dependency as well as [Markdig](https://www.nuget.org/packages/Markdig/). These can be factored out if this really took off.
--- Raydreams.HydeSharp/Program.cs
+++ Raydreams.HydeSharp/Program.cs
@@ -6,22 +6,23 @@ using Raydreams.Common.IO;
 
 namespace Raydreams.HydeSharp
 {
-    /// <summary></summary>
+    /// <summary>Main EXE class</summary>
     public class Hyde
     {
-        /// <summary></summary>
+        public static readonly string SiteFolder = "_site";
+        public static readonly string PostsFolder = "_posts";
+        public static readonly string LayoutsFolder = "_layouts";
+
+        /// <summary>Base path of the project</summary>
         private string _basePath;
 
         /// <summary></summary>
         static void Main(string[] args)
         {
-            Hyde app = new Hyde($"{IOHelpers.DesktopPath}/website/");
+            Hyde app = new Hyde( $"{IOHelpers.DesktopPath}/website/" );
+            bool res = app.Process();
 
-            string template = app.GetTemplate();
-            string[] posts = app.GetFilePaths();
-            app.WritePosts(template, posts);
-
-            Console.WriteLine("Done!");
+            Console.WriteLine( $"Sucess = {res}" );
         }
 
         /// <summary></summary>
@@ -30,10 +31,28 @@ namespace Raydreams.HydeSharp
             this._basePath = basePath;
         }
 
+        /// <summary>Lets do it</summary>
+        public bool Process()
+        {
+            try
+            {
+                string template = this.GetTemplate();
+                string[] posts = this.GetFilePaths();
+                this.WritePosts(template, posts);
+            }
+            catch (System.Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+                return false;
+            }
+
+            return true;
+        }
+
         /// <summary></summary>
         public string BasePath => this._basePath;
 
-        /// <summary></summary>
+        /// <summary>Transforms all posts to HTML</summary>
         public void WritePosts(string template, string[] posts)
         {
             MarkdownPipeline pipe = new MarkdownPipelineBuilder().UseSoftlineBreakAsHardlineBreak().Build();
@@ -49,30 +68,33 @@ namespace Raydreams.HydeSharp
                 string original = File.ReadAllText(file);
                 string html = Markdown.ToHtml(original, pipe);
 
-                string post = template.Replace(@"{{ content }}", html);
+                string post = template.Replace( @"{{ content }}", html);
 
                 string baseName = Path.GetFileNameWithoutExtension(file);
 
-                File.WriteAllText($"{this.BasePath}site/{baseName}.html", post);
+                File.WriteAllText( $"{this.BasePath}/{SiteFolder}/{baseName}.html", post);
             }
         }
 
-        /// <summary></summary>
+        /// <summary>Gets all the posts file paths</summary>
         /// <param name="path"></param>
         /// <returns></returns>
         public string[] GetFilePaths()
         {
-            string[] files = Directory.EnumerateFiles(this.BasePath + "posts", "*.md", SearchOption.TopDirectoryOnly).ToArray();
+            string[] files = Directory.EnumerateFiles( $"{this.BasePath}/{PostsFolder}", "*.md", SearchOption.TopDirectoryOnly).ToArray();
 
             return files;
         }
 
-        /// <summary></summary>
+        /// <summary>Gets a template</summary>
         /// <param name="path"></param>
         /// <returns></returns>
-        public string GetTemplate()
+        public string GetTemplate(string name = "default")
         {
-            return File.ReadAllText(this.BasePath + "_template.html");
+            if (String.IsNullOrWhiteSpace(name))
+                name = "default";
+
+            return File.ReadAllText( $"{this.BasePath}/{LayoutsFolder}/{name.Trim()}.html" );
         }
 
     }
